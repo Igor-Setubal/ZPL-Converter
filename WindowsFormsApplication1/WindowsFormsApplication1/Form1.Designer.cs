3a193111df80fa7a23121f7c5cd9a65b3bb58527
Igor-Setubal <40373414+Igor-Setubal@users.noreply.github.com>
2018-08-17 11:56:17 -0300

    Add files via upload

---
+++ WindowsFormsApplication1/WindowsFormsApplication1/Form1.Designer.cs
@@ -0,0 +1,100 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(185, 110);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(116, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Gerar etiqueta";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(255, 178);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(79, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "By Igor Setubal";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(32, 110);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(121, 23);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Selecionar etiqueta";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            this.openFileDialog1.FileOk += new System.ComponentModel.CancelEventHandler(this.openFileDialog1_FileOk);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.ClientSize = new System.Drawing.Size(337, 194);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximumSize = new System.Drawing.Size(353, 233);
+            this.MinimumSize = new System.Drawing.Size(353, 233);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "ZPL - Converter";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+    }
+}
+
---
+++ WindowsFormsApplication1/WindowsFormsApplication1/Form1.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            button1.Enabled = false;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+            System.IO.StreamReader sr = new
+            System.IO.StreamReader(openFileDialog1.FileName);
+
+            var test = (sr.ReadToEnd());
+            byte[] zpl = Encoding.UTF8.GetBytes(test);
+            //adjust print density (8dpmm), label width (4 inches), label height (6 inches), and label index (0) as necessary
+            var request = (HttpWebRequest)WebRequest.Create("http://api.labelary.com/v1/printers/8dpmm/labels/5x8/0/");
+            request.Method = "POST";
+            request.Accept = "application/pdf"; // omit this line to get PNG images back
+            request.ContentType = "application/x-www-form-urlencoded";
+            request.ContentLength = zpl.Length;
+
+            var requestStream = request.GetRequestStream();
+            requestStream.Write(zpl, 0, zpl.Length);
+            requestStream.Close();
+
+            try
+            {
+
+                var response = (HttpWebResponse)request.GetResponse();
+                var responseStream = response.GetResponseStream();
+                SaveFileDialog SaveFile = new SaveFileDialog();
+                //var fileStream = File.Create("C:\\Users\\st1ferreig\\Downloads\\lmich4.pdf"); // change file name for PNG images
+                if (SaveFile.ShowDialog() == DialogResult.OK)
+                {
+                   
+                    var fileStream = File.Create((((System.Windows.Forms.FileDialog)(SaveFile)).FileName.ToString() + ".pdf"));
+                    responseStream.CopyTo(fileStream);
+                    responseStream.Close();
+
+
+                    using (StreamWriter sw = new StreamWriter(fileStream))
+                    {
+                        sw.Write(fileStream.ToString());
+                        MessageBox.Show("Etiqueta zebra criada !", "Aviso");
+                    }
+
+                }
+
+
+            }
+
+            catch (WebException ex)
+            {
+                Console.WriteLine("Código da etiqueta errado ! Error: {0}", ex.Status);
+            }
+
+        }
+
+
+        public static Stream fileStream { get; set; }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                
+                if (openFileDialog1.ShowDialog() == DialogResult.OK)
+                {
+                    System.IO.StreamReader sr = new
+                    System.IO.StreamReader(openFileDialog1.FileName);
+                    //System.Diagnostics.Process.Start(openFileDialog1.FileName);
+
+                        button1.Enabled = true;
+
+                    sr.Close();
+
+                }
+        
+            }
+
+            catch (WebException sr) 
+            {
+                Console.WriteLine("Código da etiqueta errado ! Error: {0}", sr.Status);
+            }
+            
+        }
+
+        private void openFileDialog1_FileOk(object sender, CancelEventArgs e)
+        {
+
+        }
+
+    }
+}
+
+        
+    
+        
+    
+
